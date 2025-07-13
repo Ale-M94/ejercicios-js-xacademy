5900f7d297d10d3f7a783091d4add4bf31d8ebe6
AlejandroM94 <alemilotich.94@gmail.com>
2025-07-13 11:31:21 -0300

    Resolucion de los ejercicios de practica 1

---
+++ ejercicios-1.js
@@ -0,0 +1,60 @@
+// Ejercicio 1
+
+// function esMayor(){
+//     let edad = prompt("Ingrese su edad");
+
+//     if(edad>=18){
+//         console.log("Es mayor. Puede conducir");
+//     }else{
+//         console.log("No es mayor. No puede conducir")
+//     }
+// }
+
+
+// Ejercicio 2
+
+// function imprimirCalificacion(nota){
+//     if(nota < 3){
+//         console.log("Muy deficiente");
+//     }else if(nota >= 3 && nota < 5){
+//         console.log("Insuficiente");
+//     }else if(nota >= 5 && nota < 6){
+//         console.log("Suficiente");
+//     }else if(nota >= 6 && nota < 7){
+//         console.log("Bien");
+//     }else if(nota >= 7 && nota < 9){
+//         console.log("Notable");
+//     }else{
+//         console.log("Sobresaliente");
+//     }
+// }
+
+// let nota = prompt("Ingrese una calificación");
+
+// imprimirCalificacion(nota);
+
+
+// Ejercicio 3
+
+// for(let i=1; i<=6; i++){
+//     let numero = '';
+
+//     for(let x=0; x<i; x++){
+//         numero += i;
+//     }
+
+//     console.log(numero);
+// }
+
+
+// Ejercicio 4
+
+
+// for(let i=1; i<=6; i++){
+//     let numero = '';
+
+//     for(let x=1; x<=i; x++){
+//         numero += x;
+//     }
+//     console.log(numero);
+// }
